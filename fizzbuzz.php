c9edf312281b4a9206c550c0121d15ada7780646
bwesterg <benton.westergaard@gmail.com>
2023-08-18 14:17:08 -0600

    adds two solutions to FizzBuzz task.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,4 +1,23 @@
 <?php
+for ($i = 1; $i <= 1000; $i++) {
+    $output = "";
+
+    if ($i % 3 == 0) {
+        $output .= "Fizz";
+    }
+    if ($i % 5 == 0) {
+        $output .= "Buzz";
+    }
+    echo ($output !== "") ? $output : $i;
+    echo "\n";
+}
+?>
+
+<!-- Note for Intrado reviewer(s):
+The code below also meets the task requirements, and is perhaps more readable.
+However, I used the above solution because it uses fewer operators, and a single $output to accumulate strings "Fizz" and "Buzz" as needed. 
+The idea is to optimize the code so it runs as fast as possible. Admittedly, this is negligable given the small number range.-->
+<!-- 
     for ($i = 1; $i <= 1000; $i++)
     {
         if ( $i%3 == 0 && $i%5 == 0 )
@@ -18,4 +37,4 @@
                 echo $i."\n";
             }
     }
-?>
+-->
